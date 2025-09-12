cfad065790674e224488511ab092abd00a38556c
znorth1 <znorth@ualberta.ca>
2025-09-11 19:26:59 -0600

    A VERY rough draft of toggle buttons that will hopefully work? Please?

---
+++ CMPUT-250-Project/Assets/Scripts/TabScript.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabScript : MonoBehaviour
+{
+    public ToggleGroup toggleGroup;
+    public Toggle banRequests;
+    public Toggle Rules;
+    public Toggle DMS;
+    // Start is called before the first frame update
+    void Start()
+    {
+        toggleGroup.RegisterToggle(banRequests);
+        toggleGroup.RegisterToggle(Rules);
+        toggleGroup.RegisterToggle(DMS);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void ToggleValueChanged(Toggle change)
+    {
+        
+    }
+}
