68fbd141f464016a3479a9edfe1c5e6f736e7689
znorth1 <znorth@ualberta.ca>
2025-09-12 22:54:33 -0600

    trying to work on a listener for these toggles, will research more tomorrow. Also added ToggleUI to hopefully use to change 'windows'

--- CMPUT-250-Project/Assets/Scripts/TabScript.cs
+++ CMPUT-250-Project/Assets/Scripts/TabScript.cs
@@ -2,25 +2,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class TabScript : MonoBehaviour
 {
-    public GameObject RulesPage;
-    public GameObject AppealsPage;
-    public GameObject DMsPage;
+    Toggle banAppeal;
+    Toggle rules;
+    Toggle dms;
+    public static ToggleGroup toggleGroup;
+    protected int currentTabIndex { get; set; }
+    Toggle[] toggles = toggleGroup.GetComponentsInChildren<Toggle>();
+    ToggleUI uiSwitch = new ToggleUI();
+
     // Start is called before the first frame update
     void Start()
     {
-
         
+        int i = 0;
+        foreach (Toggle toggle in toggles) {
+            toggle.onValueChanged.AddListener(delegate { SetTabState(toggle, toggle.isOn); });
+        }
 
+    }
+    protected void SetTabState(Toggle toggle, bool isSelected)
+    {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
+
