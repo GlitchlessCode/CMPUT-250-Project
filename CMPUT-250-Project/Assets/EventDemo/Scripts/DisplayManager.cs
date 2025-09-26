53572a269094398edbdc57d07f27f6420d00a8a3
GlitchlessCode <talyn.veugelers@gmail.com>
2025-09-25 21:35:48 -0600

    Add Demo Scene for GameEvent<T> System

---
+++ CMPUT-250-Project/Assets/EventDemo/Scripts/DisplayManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayManager : MonoBehaviour
+{
+    public StringGameEvent RecieveText;
+    public UnitGameEvent ClearText;
+    public IntGameEvent AddAmount;
+
+    public Text TextDisplay;
+    public Text CounterDisplay;
+
+    private int counter = 0;
+
+    public void Awake()
+    {
+        RecieveText.Subscribe(OnRecieveText);
+        ClearText.Subscribe(OnClearText);
+        AddAmount.Subscribe(OnAddAmount);
+    }
+
+    private void setText(string text)
+    {
+        if (TextDisplay != null)
+        {
+            TextDisplay.text = text;
+        }
+    }
+
+    private void setCounter()
+    {
+        if (CounterDisplay != null)
+        {
+            CounterDisplay.text = counter.ToString();
+        }
+    }
+
+    private void OnRecieveText(string text)
+    {
+        setText(text);
+        counter += 1;
+        setCounter();
+    }
+
+    private void OnClearText()
+    {
+        setText("");
+    }
+
+    private void OnAddAmount(int amount)
+    {
+        counter += amount;
+        setCounter();
+    }
+}
---
+++ CMPUT-250-Project/Assets/EventDemo/Scripts/InputManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InputManager : MonoBehaviour
+{
+    public StringGameEvent SubmitEvent;
+    public UnitGameEvent ClearEvent;
+    public IntGameEvent AddEvent;
+
+    public InputField InputBox;
+
+    public void SubmitText()
+    {
+        if (InputBox != null)
+        {
+            string text = InputBox.text;
+            InputBox.text = "";
+            SubmitEvent.Emit(text);
+        }
+    }
+
+    public void ClearText()
+    {
+        ClearEvent.Emit();
+    }
+
+    public void Add(int amount)
+    {
+        AddEvent.Emit(amount);
+    }
+}
