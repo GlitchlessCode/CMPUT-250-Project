c8be6bdc621b0e94209418ff2b3e4a831b4ba9b4
mannsber <mannsber@ualberta.ca>
2025-10-09 15:39:54 -0600

    Implemented EOD button and scoring for multi day

---
+++ CMPUT-250-Project/Assets/Scripts/EOD/EODPromptController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EODPromptController : Subscriber   // same base class style as your other systems
+{
+    [SerializeField] private GameObject arrowPrompt; // your arrow or hint UI
+    [SerializeField] private Button powerButton;     // your Power button
+
+    [Header("Events")]
+    public UnitGameEvent DayFinished; // assign this in Inspector
+
+    protected override void Subscribe()
+    {
+        DayFinished?.Subscribe(OnDayFinished);
+    }
+
+    private void Start()
+    {
+        if (arrowPrompt) arrowPrompt.SetActive(false);
+        if (powerButton) powerButton.interactable = false;
+    }
+
+    private void OnDayFinished()
+    {
+        if (arrowPrompt) arrowPrompt.SetActive(true);
+        if (powerButton) powerButton.interactable = true;
+    }
+}
