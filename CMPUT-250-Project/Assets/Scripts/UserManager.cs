b8e0b5dd3153b93bd600cac820dcb96ebaa13374
Miles Taylor <milesmandevilla@gmail.com>
2025-10-10 15:48:39 -0600

    Rules fix (#28)
    * rules fix
    
    * Put new rules on rules tab (temp)
    
    * update rules text
    
    * Update controllers, fix scoring
    
    ---------
    
    Co-authored-by: GlitchlessCode <talyn.veugelers@gmail.com>

--- CMPUT-250-Project/Assets/Scripts/UserManager.cs
+++ CMPUT-250-Project/Assets/Scripts/UserManager.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 class InternalDayDefinition
@@ -141,6 +142,8 @@ public class UserManager : Subscriber
             Path.Combine("lang", "en", "days", day.Directory)
         );
 
+        // (string ruleName, string checking, string ruleType, var criteria)
+
         currentUser = null;
         MoveToNextUser();
     }
@@ -188,9 +191,45 @@ public class UserManager : Subscriber
         UserEntry? user = currentUser;
         if (user != null)
         {
-            bool success = true; // FIXME: Add rule checks
+            bool passes = true;
+            int numMsg = 0;
 
-            AfterAppeal?.Emit(success);
+            foreach (string message in user.Value.messages)
+            {
+                if (message.Length > 50)
+                {
+                    passes = false;
+                }
+                numMsg++;
+                if (Regex.IsMatch(message.ToLower(), @".*cat.*"))
+                {
+                    passes = false;
+                }
+                if (Regex.IsMatch(message.ToLower(), @".*(.)\1{2,}.*"))
+                {
+                    passes = false;
+                }
+            }
+
+            if (user.Value.appeal_message == "")
+            {
+                passes = false;
+            }
+            if (user.Value.name.Length > 12)
+            {
+                passes = false;
+            }
+            if (user.Value.bio.Split(' ').Length < 4)
+            {
+                passes = false;
+            }
+
+            if (numMsg < 5)
+            {
+                passes = false;
+            }
+
+            AfterAppeal?.Emit(passes == decision);
         }
 
         MoveToNextUser();
