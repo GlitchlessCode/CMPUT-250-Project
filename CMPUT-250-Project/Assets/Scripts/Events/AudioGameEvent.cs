b0516b809fdbce56e77d01cb52cfa0a3c9d34fbc
GlitchlessCode <talyn.veugelers@gmail.com>
2025-10-09 20:55:31 -0600

    Add AudioManager and AudioBuses, add hover and press sounds

---
+++ CMPUT-250-Project/Assets/Scripts/Events/AudioGameEvent.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public struct Audio
+{
+    public AudioClip clip;
+
+    [Range(0.0f, 1.0f)]
+    public float volume;
+
+    [Range(0.0f, 3.0f)]
+    public float pitchVariance;
+
+    public bool loop;
+
+    public Audio(AudioClip clipIn, float volumeIn, float pitchVarianceIn, bool loopIn)
+    {
+        clip = clipIn;
+        volume = volumeIn;
+        pitchVariance = pitchVarianceIn;
+        loop = loopIn;
+    }
+}
+
+[CreateAssetMenu(menuName = "Game Events/AudioGameEvent")]
+public class AudioGameEvent : GameEvent<Audio> { }
---
+++ CMPUT-250-Project/Assets/Scripts/Events/FloatGameEvent.cs
@@ -0,0 +1,4 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game Events/FloatGameEvent")]
+public class FloatGameEvent : GameEvent<float> { }
