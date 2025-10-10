c191eaae2038de040c521612caed0ed323d5f0b4
znorth1 <156261606+znorth1@users.noreply.github.com>
2025-10-09 22:10:44 -0600

    Added Settings Tab and Sliders, with a SettingsController (#18)
    * Working settings tab, need to work on slider functionality and visibility
    
    * Added TextMeshPro for my dynamic float displays for the sliders. Slider rough visuals and slider frontend are functional
    
    * Backend of sliders working, placeholder 'UI' element is fine for now
    
    * Just making it so the default tab is the moderation one again!
    
    * Update and improve SettingsController
    
    ---------
    
    Co-authored-by: GlitchlessCode <talyn.veugelers@gmail.com>

---
+++ CMPUT-250-Project/Assets/Scripts/SettingsController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsController : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI musicSliderText = null;
+
+    [SerializeField]
+    private TextMeshProUGUI soundSliderText = null;
+
+    [SerializeField]
+    private float maxSliderAmount = 100.0f;
+
+    [Header("Events")]
+    public FloatGameEvent ChangeMusic;
+    public FloatGameEvent ChangeSound;
+
+    public void MusicValueChange(float value)
+    {
+        changeValue(value, ChangeMusic, musicSliderText);
+    }
+
+    public void SoundValueChange(float value)
+    {
+        changeValue(value, ChangeSound, soundSliderText);
+    }
+
+    private void changeValue(float value, FloatGameEvent channel, TextMeshProUGUI sliderText)
+    {
+        channel?.Emit(value);
+        if (sliderText != null)
+        {
+            float localValue = value * maxSliderAmount;
+            sliderText.text = localValue.ToString("0");
+        }
+    }
+}
