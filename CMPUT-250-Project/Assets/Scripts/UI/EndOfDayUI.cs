0570b9413a2b06148ab81b712eca62ccb267c2fe
mannsber <mannsber@ualberta.ca>
2025-10-10 11:24:49 -0600

    Finished power button and EOD scene, bugs present

---
+++ CMPUT-250-Project/Assets/Scripts/UI/EndOfDayUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndOfDayUI : MonoBehaviour
+{
+    [SerializeField] private Text dayScoreText;
+    [SerializeField] private Text appealsText;
+    [SerializeField] private Text totalScoreText;
+
+    void Start()
+    {
+        var sm = FindObjectOfType<ScoreManager>();
+        if (sm == null)
+        {
+            if (dayScoreText) dayScoreText.text = "Score: N/A";
+            if (appealsText) appealsText.text = "Appeals: N/A";
+            if (totalScoreText) totalScoreText.text = "Total Score: N/A";
+            Debug.LogWarning("EndOfDayUI: ScoreManager not found. Is it marked DontDestroyOnLoad?");
+            return;
+        }
+
+        int dayScore = sm.GetDayScore();
+        int appeals = sm.GetAppealsProcessed();
+        int totalScore = sm.GetTotalScoreSoFar();
+
+        if (dayScoreText) dayScoreText.text = $"Day {sm.CurrentDayIndex} Score: {dayScore}";
+        if (appealsText) appealsText.text = $"Appeals Processed: {appeals}";
+        if (totalScoreText) totalScoreText.text = $"Total Score (So Far): {totalScore}";
+    }
+}
+
