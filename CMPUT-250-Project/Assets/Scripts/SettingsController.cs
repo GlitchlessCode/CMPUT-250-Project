84f4dd019be452c57e719f32b38b4e34a3241c1d
znorth1 <znorth@ualberta.ca>
2025-10-09 19:30:11 -0600

    Added TextMeshPro for my dynamic float displays for the sliders. Slider rough visuals and slider frontend are functional

---
+++ CMPUT-250-Project/Assets/Scripts/SettingsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SettingsController : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI musicSliderText = null;
+    [SerializeField] private TextMeshProUGUI soundSliderText = null;
+
+    [SerializeField] private float maxSliderAmount = 100.0f;
+
+    public void MusicSliderChange(float value)
+    {
+        float localValue = value * maxSliderAmount;
+        musicSliderText.text = localValue.ToString("0");
+    }
+
+    public void SoundSliderChange(float value)
+    {
+        float localValue = value * maxSliderAmount;
+        soundSliderText.text = localValue.ToString("0");
+    }
+}
