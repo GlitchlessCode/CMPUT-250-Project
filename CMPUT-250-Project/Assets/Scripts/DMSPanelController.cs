a49efc2ec20eec5c8caeeafb505c37773ed49b49
Miles Taylor <milesmandevilla@gmail.com>
2025-10-10 21:17:33 -0600

    Add DM Tab (#29)
    * Put new rules on rules tab (temp)
    
    * rules fix
    
    * prepping to get DMs working
    
    * make dm panel controller outline
    
    * resizeable box
    
    * makes multiple chats show up - just not in order
    
    * messages scroll now - still not hooked up to event
    
    * scrolling!
    
    * set up for DMSent
    
    * lowered maax height for DM
    
    * Move DM panel controller to UIController
    
    ---------
    
    Co-authored-by: GlitchlessCode <talyn.veugelers@gmail.com>

---
+++ CMPUT-250-Project/Assets/Scripts/DMSPanelController.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DMSPanelController : Subscriber
+{
+
+    [Header("Event Listeners")]
+    public DirectMessageGameEvent DMSent;
+
+    [SerializeField] private TextMeshProUGUI textComponent;
+    [SerializeField] private Image image;
+
+    public GameObject container;
+    public Transform DMPanel;
+    private List<GameObject> containers = new List<GameObject>();
+    private List<RectTransform> transforms = new List<RectTransform>();
+    private List<float> heights = new List<float>();
+    private float height = 0;
+    private float lastHeight =0;
+
+    int o = 1;
+    
+
+    void Start ()
+    {
+        
+    }
+
+    protected override void Subscribe()
+    {
+        DMSent?.Subscribe(OnDMSent);
+    }
+
+    public void OnDMSent(DirectMessage DM)
+    {
+        AddDM(DM);
+        killDM();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void AddDM(DirectMessage DM)
+    {
+
+        if (container != null && DMPanel != null)
+        { 
+            GameObject instantiatedObject = Instantiate(container, DMPanel);
+            textComponent = instantiatedObject.GetComponentInChildren<TextMeshProUGUI>();
+            textComponent.text = DM.message;
+            
+
+            containers.Add(instantiatedObject);
+            
+            RectTransform trans = instantiatedObject.GetComponent<RectTransform>();
+
+            transforms.Add(trans);
+            Canvas.ForceUpdateCanvases();
+            heights.Add(trans.rect.height);
+
+            foreach (GameObject con in containers)
+            {
+                height = 0;
+                int currentIndex = containers.IndexOf(con); 
+                
+                 
+                 for (int i = 0; i <= currentIndex; i++)
+                 {
+                    height += heights[i];
+
+                    Vector2 currentPosition = transforms[currentIndex].anchoredPosition;
+
+                    currentPosition = new Vector2(currentPosition.x, currentPosition.y - height + 20);
+
+                    transforms[i].anchoredPosition = currentPosition;
+
+                    lastHeight = transforms[currentIndex].rect.height;
+
+                 }
+
+                if (transforms.Count == 1)
+                {
+                    Vector2 currentPosition = transforms[0].anchoredPosition;
+                    currentPosition = new Vector2(currentPosition.x, currentPosition.y+height - 20);
+                    transforms[0].anchoredPosition = currentPosition;
+                    
+                }
+
+                for (int i = 0; i < transforms.Count-1; i++)
+                {
+                    Vector2 currentPosition = transforms[i].anchoredPosition;
+                    currentPosition = new Vector2(currentPosition.x, currentPosition.y+height-lastHeight-20);
+                    transforms[i].anchoredPosition = currentPosition;
+
+                }
+                
+            }
+            o++;
+            
+
+        }
+    }
+
+    void killDM()
+    {
+        foreach (GameObject con in containers)
+        {
+            if(con.transform.position.y > 600)
+            {
+                Image image = con.GetComponentInChildren<Image>();
+                textComponent = con.GetComponentInChildren<TextMeshProUGUI>();
+                image.enabled = false;
+                textComponent.enabled = false;
+            }
+        }
+    }
+
+
+}
---
+++ CMPUT-250-Project/Assets/Scripts/Events/DMGameEvent.cs
@@ -0,0 +1,4 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game Events/DMGameEvent")]
+public class DMGameEvent : GameEvent<DirectMessage> { }
