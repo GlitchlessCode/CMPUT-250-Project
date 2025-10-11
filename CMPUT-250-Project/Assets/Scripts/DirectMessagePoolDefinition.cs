71971f92b90dedaeeb036600d5dafb99798915dd
GlitchlessCode <talyn.veugelers@gmail.com>
2025-10-10 20:58:33 -0600

    Add DM Files, add DM Manager, definitions
    + Adds DM Manager script
    + Adds DM Pool and Sequence Definition ScriptableObjects
    + Adds DM Messages for day 1

---
+++ CMPUT-250-Project/Assets/Scripts/DirectMessagePoolDefinition.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Direct Messages/Pool")]
+public class DirectMessagePoolDefinition : ScriptableObject
+{
+    public string directory;
+    public List<string> dmFiles;
+
+    public List<DirectMessage> GetMessages()
+    {
+        try
+        {
+            System.Random rand = new System.Random();
+
+            Dictionary<string, DirectMessage> files = JSONImporter.ImportDirectory<DirectMessage>(
+                Path.Combine("lang", "en", "messages", directory)
+            );
+
+            List<DirectMessage> messages = new List<DirectMessage>();
+            foreach (string filename in dmFiles)
+            {
+                messages.Add(files[filename]);
+            }
+
+            return messages;
+        }
+        catch (Exception err)
+        {
+            Debug.LogWarning("Failed to load DirectMessagePool " + directory + " due to " + err);
+            return new List<DirectMessage>();
+        }
+    }
+}
---
+++ CMPUT-250-Project/Assets/Scripts/DirectMessageSequenceDefinition.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Direct Messages/Sequence")]
+public class DirectMessageSequenceDefinition : ScriptableObject
+{
+    public string directory;
+    public List<string> dmFiles;
+
+    public List<DirectMessage> GetMessages()
+    {
+        try
+        {
+            System.Random rand = new System.Random();
+
+            Dictionary<string, DirectMessage> files = JSONImporter.ImportDirectory<DirectMessage>(
+                Path.Combine("lang", "en", "messages", directory)
+            );
+
+            List<DirectMessage> messages = new List<DirectMessage>();
+            foreach (string filename in dmFiles)
+            {
+                messages.Add(files[filename]);
+            }
+
+            return messages;
+        }
+        catch (Exception err)
+        {
+            Debug.LogWarning(
+                "Failed to load DirectMessageSequence " + directory + " due to " + err
+            );
+            return new List<DirectMessage>();
+        }
+    }
+}
