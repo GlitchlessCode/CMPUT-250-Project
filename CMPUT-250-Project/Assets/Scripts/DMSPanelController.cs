e199f6c12d73556bc7a744f43f16bb3611876776
Miles Taylor <milesmandevilla@gmail.com>
2025-10-13 17:40:05 -0600

    Fix dm (#32)
    * scrolls just in the wrong way
    
    * scrolls the right way, added delay to nofic
    
    * update dms every frame

--- CMPUT-250-Project/Assets/Scripts/DMSPanelController.cs
+++ CMPUT-250-Project/Assets/Scripts/DMSPanelController.cs
@@ -9,6 +9,7 @@ public class DMSPanelController : Subscriber
 
     [Header("Event Listeners")]
     public DirectMessageGameEvent DMSent;
+    public BoolGameEvent DMTabClick;
 
     [SerializeField] private TextMeshProUGUI textComponent;
     [SerializeField] private Image image;
@@ -18,10 +19,6 @@ public class DMSPanelController : Subscriber
     private List<GameObject> containers = new List<GameObject>();
     private List<RectTransform> transforms = new List<RectTransform>();
     private List<float> heights = new List<float>();
-    private float height = 0;
-    private float lastHeight =0;
-
-    int o = 1;
     
 
     void Start ()
@@ -32,93 +29,77 @@ public class DMSPanelController : Subscriber
     protected override void Subscribe()
     {
         DMSent?.Subscribe(OnDMSent);
+        DMTabClick?.Subscribe(OnDMTabClick);
     }
 
     public void OnDMSent(DirectMessage DM)
     {
         AddDM(DM);
-        killDM();
+        // killDM();
+    }
+
+    public void OnDMTabClick(bool clicked)
+    {
+        
     }
 
     void Update()
     {
-
+        LayoutRebuilder.ForceRebuildLayoutImmediate(DMPanel.GetComponent<RectTransform>());
     }
 
     void AddDM(DirectMessage DM)
     {
 
-        if (container != null && DMPanel != null)
-        { 
+        // if (container != null && DMPanel != null)
+        // { 
             GameObject instantiatedObject = Instantiate(container, DMPanel);
             textComponent = instantiatedObject.GetComponentInChildren<TextMeshProUGUI>();
             textComponent.text = DM.message;
             
 
             containers.Add(instantiatedObject);
+
+            
             
             RectTransform trans = instantiatedObject.GetComponent<RectTransform>();
 
-            transforms.Add(trans);
             Canvas.ForceUpdateCanvases();
-            heights.Add(trans.rect.height);
+            LayoutRebuilder.ForceRebuildLayoutImmediate(DMPanel.GetComponent<RectTransform>());
+            transforms.Add(trans);
+            // heights.Add(trans.rect.height);
 
-            foreach (GameObject con in containers)
-            {
-                height = 0;
-                int currentIndex = containers.IndexOf(con); 
-                
-                 
-                 for (int i = 0; i <= currentIndex; i++)
-                 {
-                    height += heights[i];
+            // foreach (GameObject con in containers)
+            // {
+            //     int currentIndex = containers.IndexOf(con);
 
-                    Vector2 currentPosition = transforms[currentIndex].anchoredPosition;
+            //     if (currentIndex != 0){
+            //         transforms[currentIndex].anchoredPosition = new Vector2( 
+            //             transforms[currentIndex].anchoredPosition.x,
+            //             transforms[currentIndex].anchoredPosition.y + transforms[currentIndex-1].sizeDelta.y
+            //         );
 
-                    currentPosition = new Vector2(currentPosition.x, currentPosition.y - height + 20);
-
-                    transforms[i].anchoredPosition = currentPosition;
-
-                    lastHeight = transforms[currentIndex].rect.height;
-
-                 }
-
-                if (transforms.Count == 1)
-                {
-                    Vector2 currentPosition = transforms[0].anchoredPosition;
-                    currentPosition = new Vector2(currentPosition.x, currentPosition.y+height - 20);
-                    transforms[0].anchoredPosition = currentPosition;
-                    
-                }
-
-                for (int i = 0; i < transforms.Count-1; i++)
-                {
-                    Vector2 currentPosition = transforms[i].anchoredPosition;
-                    currentPosition = new Vector2(currentPosition.x, currentPosition.y+height-lastHeight-20);
-                    transforms[i].anchoredPosition = currentPosition;
-
-                }
-                
-            }
-            o++;
+            //         Debug.Log(""+transforms[currentIndex-1].sizeDelta.y);
+            //     }
+            // }
             
 
-        }
+        // }
     }
 
-    void killDM()
-    {
-        foreach (GameObject con in containers)
-        {
-            if(con.transform.position.y > 600)
-            {
-                Image image = con.GetComponentInChildren<Image>();
-                textComponent = con.GetComponentInChildren<TextMeshProUGUI>();
-                image.enabled = false;
-                textComponent.enabled = false;
-            }
-        }
-    }
+    // void killDM()
+    // {
+    //     foreach (GameObject con in containers)
+    //     {
+    //         if(con.transform.position.y > 600)
+    //         {
+    //             Image image = con.GetComponentInChildren<Image>();
+    //             textComponent = con.GetComponentInChildren<TextMeshProUGUI>();
+    //             image.enabled = false;
+    //             textComponent.enabled = false;
+    //         }
+    //     }
+    // }
 
 
 }
