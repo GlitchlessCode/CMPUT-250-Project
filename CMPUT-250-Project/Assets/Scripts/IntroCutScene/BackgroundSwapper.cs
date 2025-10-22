1a812d2ce324371bd5f3acbcce02ee31441dc754
znorth1 <156261606+znorth1@users.noreply.github.com>
2025-10-22 15:56:58 -0600

    Main menu completed (#54)
    * Main menu MADE!
    
    * Fix Button Raycasting, Add TitleScreen To Build
    
    ---------
    
    Co-authored-by: GlitchlessCode <talyn.veugelers@gmail.com>

--- CMPUT-250-Project/Assets/Scripts/IntroCutScene/BackgroundSwapper.cs
+++ CMPUT-250-Project/Assets/Scripts/IntroCutScene/BackgroundSwapper.cs
@@ -4,17 +4,26 @@ using System.Collections;
 
 public class BackgroundSwapper : MonoBehaviour
 {
+    [Header("Image")]
     public Image backgroundImage;  // assign your UI Image here
+
+    [Header("Sprites")]
     public Sprite sprite1;
     public Sprite sprite2;
+
+    [Header("Swap Interval")]
     public float swapInterval = 3f;  // seconds between swaps
 
+    [Header("Panel")]
+    public GameObject PowerOn;
+      
     private bool showingSprite1 = true;
 
     void Start()
     {
         // Set starting image
         backgroundImage.sprite = sprite1;
+        PowerOn.GetComponent<Animator>().SetTrigger("PlayPowerOn");
         StartCoroutine(SwapRoutine());
     }
 
