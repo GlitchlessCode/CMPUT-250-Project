5137bfdb4b53fc3d76e0f960a90493ba1e780f7f
znorth1 <znorth@ualberta.ca>
2025-10-22 14:26:39 -0600

    Main menu MADE!

---
+++ CMPUT-250-Project/Assets/Scripts/UI/TitleScreenUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleScreenUI : MonoBehaviour
+{
+    [Header("Panels")]
+    public GameObject PowerOff;
+    
+    [Header("Buttons")]
+    public Button StartButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PowerOff.SetActive(false);
+        StartButton.onClick.AddListener(OnStartButton);
+    }
+
+    private void OnStartButton()
+    {
+        PowerOff.SetActive(true);
+        PowerOff.GetComponent<Animator>().SetTrigger("PlayPowerOff");       
+    }
+}
