be5c758cab03811e585faacc0867480bfe9e6eb7
mannsber <93225688+mannsber@users.noreply.github.com>
2025-10-21 20:25:06 -0600

    Intro cut scene (#51)
    * Intro cut scene made, button bug to be fixed
    
    * Intro cut scene completed
    
    ---------
    
    Co-authored-by: Zach North <znorth@ualberta.ca> (all I did was fix a merge conflict)

---
+++ CMPUT-250-Project/Assets/Scripts/IntroCutScene/BackgroundSwapper.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BackgroundSwapper : MonoBehaviour
+{
+    public Image backgroundImage;  // assign your UI Image here
+    public Sprite sprite1;
+    public Sprite sprite2;
+    public float swapInterval = 3f;  // seconds between swaps
+
+    private bool showingSprite1 = true;
+
+    void Start()
+    {
+        // Set starting image
+        backgroundImage.sprite = sprite1;
+        StartCoroutine(SwapRoutine());
+    }
+
+    IEnumerator SwapRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(swapInterval);
+
+            showingSprite1 = !showingSprite1;
+            backgroundImage.sprite = showingSprite1 ? sprite1 : sprite2;
+        }
+    }
+}
