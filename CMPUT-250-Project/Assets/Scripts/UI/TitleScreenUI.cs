db188c6becf92c534b8c91dbe8d637276fd3b7ce
GlitchlessCode <talyn.veugelers@gmail.com>
2025-10-22 18:56:42 -0600

    Add main menu audio (#55)

--- CMPUT-250-Project/Assets/Scripts/UI/TitleScreenUI.cs
+++ CMPUT-250-Project/Assets/Scripts/UI/TitleScreenUI.cs
@@ -8,9 +8,15 @@ public class TitleScreenUI : MonoBehaviour
 {
     [Header("Panels")]
     public GameObject PowerOff;
-    
+
     [Header("Buttons")]
     public Button StartButton;
+
+    [Header("Audio")]
+    public Audio ShutdownAudio;
+
+    [Header("Events")]
+    public AudioGameEvent AudioBus;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +27,17 @@ public class TitleScreenUI : MonoBehaviour
 
     private void OnStartButton()
     {
+        StartCoroutine(Shutdown());
+    }
+
+    private IEnumerator Shutdown()
+    {
+        yield return new WaitForSeconds(0.15f);
+        if (ShutdownAudio.clip != null)
+        {
+            AudioBus?.Emit(ShutdownAudio);
+        }
         PowerOff.SetActive(true);
-        PowerOff.GetComponent<Animator>().SetTrigger("PlayPowerOff");       
+        PowerOff.GetComponent<Animator>().SetTrigger("PlayPowerOff");
     }
 }
