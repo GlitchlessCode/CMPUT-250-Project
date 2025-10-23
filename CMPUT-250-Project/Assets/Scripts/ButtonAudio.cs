0f6969ecf99d5e170855f56d17c66ce12823d9c6
GlitchlessCode <talyn.veugelers@gmail.com>
2025-10-22 18:24:36 -0600

    Add main menu audio

---
+++ CMPUT-250-Project/Assets/Scripts/ButtonAudio.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonAudio : MonoBehaviour
+{
+    public Audio Hover;
+    public Audio ClickStart;
+    public Audio ClickRelease;
+
+    [Header("Events")]
+    public AudioGameEvent AudioBus;
+
+    void Awake()
+    {
+        gameObject.TryGetComponent(out EventTrigger trigger);
+        if (trigger == null)
+        {
+            trigger = gameObject.AddComponent<EventTrigger>();
+        }
+        if (Hover.clip != null)
+        {
+            trigger.triggers.Add(createEntry(EventTriggerType.PointerEnter, OnHover));
+        }
+        if (ClickStart.clip != null)
+        {
+            trigger.triggers.Add(createEntry(EventTriggerType.PointerDown, OnClickStart));
+        }
+        if (ClickRelease.clip != null)
+        {
+            trigger.triggers.Add(createEntry(EventTriggerType.PointerUp, OnClickRelease));
+        }
+    }
+
+    private void OnHover(PointerEventData _)
+    {
+        AudioBus?.Emit(Hover);
+    }
+
+    private void OnClickStart(PointerEventData _)
+    {
+        AudioBus?.Emit(ClickRelease);
+    }
+
+    private void OnClickRelease(PointerEventData _)
+    {
+        AudioBus?.Emit(ClickStart);
+    }
+
+    private EventTrigger.Entry createEntry(EventTriggerType kind, Action<PointerEventData> callback)
+    {
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.callback.AddListener((data) => callback((PointerEventData)data));
+        entry.eventID = kind;
+        return entry;
+    }
+}
