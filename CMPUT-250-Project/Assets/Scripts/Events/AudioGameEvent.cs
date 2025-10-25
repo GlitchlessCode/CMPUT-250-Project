1d94d1a755ffc5e96b52035ecef77e3eac5d6480
GlitchlessCode <talyn.veugelers@gmail.com>
2025-10-24 18:32:05 -0600

    Add final vertslice audio
    + Adds tally-up
    + Adds EOD, Endscreen and Failstate background music
    + Fixes loop not working

--- CMPUT-250-Project/Assets/Scripts/Events/AudioGameEvent.cs
+++ CMPUT-250-Project/Assets/Scripts/Events/AudioGameEvent.cs
@@ -1,6 +1,21 @@
 ﻿using UnityEngine;
 
 [System.Serializable]
+public struct PitchOverride
+{
+    public bool active;
+
+    [Range(0.0f, 5.0f)]
+    public float value;
+
+    public PitchOverride(bool activeIn, float valueIn)
+    {
+        active = activeIn;
+        value = valueIn;
+    }
+}
+
+[System.Serializable]
 public struct Audio
 {
     public AudioClip clip;
@@ -13,12 +28,15 @@ public struct Audio
 
     public bool loop;
 
+    public PitchOverride pitchOverride;
+
     public Audio(AudioClip clipIn, float volumeIn, float pitchVarianceIn, bool loopIn)
     {
         clip = clipIn;
         volume = volumeIn;
         pitchVariance = pitchVarianceIn;
         loop = loopIn;
+        pitchOverride = new PitchOverride(false, 1.0f);
     }
 }
 
