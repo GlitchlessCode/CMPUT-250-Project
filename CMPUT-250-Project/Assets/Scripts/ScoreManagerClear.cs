25f5124377a2ee2b6314ddc68638a821a1f25071
GlitchlessCode <talyn.veugelers@gmail.com>
2025-11-07 22:18:06 -0700

    Fix Play Again & ScoreManager Existance (#78)

---
+++ CMPUT-250-Project/Assets/Scripts/ScoreManagerClear.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManagerClear : MonoBehaviour
+{
+    void Start()
+    {
+        foreach (ScoreManager manager in FindObjectsOfType<ScoreManager>())
+        {
+            Destroy(manager.gameObject);
+        }
+        Destroy(gameObject);
+    }
+}
