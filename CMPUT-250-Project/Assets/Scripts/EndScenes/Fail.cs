5279f1ee8f34831cc108e3300c107fd2fb000eb0
Miles Taylor <milesmandevilla@gmail.com>
2025-11-13 12:22:39 -0700

    Emails (#83)
    * figured out how to get images inside scrollview
    
    * basic intro email layout
    
    * update email scrolling to work with game objects
    
    * setup failstate email template
    
    * failstate email + animation
    
    * add keyboard scrolling

---
+++ CMPUT-250-Project/Assets/Scripts/EndScenes/Fail.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Fail : Subscriber
+{
+    [Header("Scroll")]
+    public EmailScroller email;
+    public List<GameObject> emailObjects;
+    public GameObject button;
+
+    void Start()
+    {
+        foreach (GameObject thing in emailObjects)
+        {
+            email.updateObject(thing);
+        }
+
+        button.transform.SetAsLastSibling();
+    }
+
+    void Update(){
+        email.scroll();
+    }
+
+}
+
