574f877ec5765e997f291fd0360ee4dc4a857576
mannsber <93225688+mannsber@users.noreply.github.com>
2025-11-16 21:58:08 -0700

    Day 2 rules implemented (#92)
    * Day 2 rules implemented
    
    * Fixed issues pointed out in previous PR
    
    * Update Rulesets.cs
    
    Fixed missing "!"

--- CMPUT-250-Project/Assets/Scripts/Rulesets.cs
+++ CMPUT-250-Project/Assets/Scripts/Rulesets.cs
@@ -62,6 +62,76 @@ public class Day1Rules : IRuleset
     }
 }
 
+public class Day2Rules : IRuleset
+{
+    public void AddRules(Validator validator)
+    {
+        validator.AddCondition(
+            "1. NO swearing allowed in chat!",
+            (currentUser) =>
+            {
+                return validator.messageRepeatsSpecific(currentUser, 2, @"\*");
+            }
+        );
+
+        validator.AddCondition(
+            "2. ban appeal MUST exist!",
+            (currentUser) =>
+            {
+                return validator.stringLengthCheck(currentUser.Value.appeal_message, ">", 0);
+            }
+        );
+
+        validator.AddCondition(
+            "3. MINIMUM 3 words in each chat message!",
+            (currentUser) =>
+            {
+                return validator.wordsPerMessage(currentUser, ">=", 3);
+            }
+        );
+
+        validator.AddCondition(
+            "4. no full chat logs in ENTIRELY lowercase!",
+            (currentUser) =>
+            {
+                return !validator.messagesContain(currentUser, @"[A-Z]");
+            }
+        );
+
+        validator.AddCondition(
+            "5. do NOT share personal information!",
+            (currentUser) =>
+            {
+                foreach (string msg in currentUser.Value.messages)
+                {
+                    if (Regex.IsMatch(msg.ToLower(), @"[0-9]{3}"))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        );
+
+        validator.AddCondition(
+            "6. NO links in user bios or chat!",
+            (currentUser) =>
+            {
+                return validator.messagesContain(currentUser, @"https?://");
+            }
+        );
+
+        validator.AddCondition(
+            "7. pls unban ALL users that have been banned for over a month!",
+            (currentUser) =>
+            {
+                return true;
+            }
+        );
+    }
+}
+
+
 public class Day3Rules : IRuleset
 {
     public void AddRules(Validator validator)
